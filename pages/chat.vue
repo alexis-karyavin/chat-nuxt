1067652bda0823650ab6b7f05da9c06a174c57ba
alexis-karyavin <alexis-karyavin@yandex.ru>
2020-05-20 15:13:58 +0300

    add chat page, redirect no authorization users

---
+++ pages/chat.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>Chat Page {{ user.name }}</h1>
+
+</template>
+
+<script>
+  import {mapState} from 'vuex';
+  export default {
+    name: "chat",
+    middleware: ['chat'],
+    computed: mapState(["user"]),
+  }
+</script>
+
+<style scoped>
+
+</style>
