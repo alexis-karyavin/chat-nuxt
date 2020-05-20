568d5b5ba269ca1115b903c6b61dfeaff7034fa2
alexis-karyavin <alexis-karyavin@yandex.ru>
2020-05-20 14:28:52 +0300

    Add empty layout and add form authorization

--- pages/index.vue
+++ pages/index.vue
@@ -1,26 +1,74 @@
 <template>
-  <v-layout>
-    <v-flex>
-      <v-btn @click="message">New Message</v-btn>
+  <v-layout column justify-center align-center>
+    <v-flex xs12 sm8>
+      <v-card min-width="400">
+        <v-card-title>
+          <h1>Nuxt Chat</h1>
+        </v-card-title>
+        <v-card-text>
+          <v-form
+            ref="form"
+            v-model="valid"
+            lazy-validation
+          >
+            <v-text-field
+              v-model="name"
+              :counter="10"
+              :rules="nameRules"
+              label="Name"
+              required
+            ></v-text-field>
+
+            <v-text-field
+              v-model="room"
+              :rules="roomRules"
+              label="Введите комнату"
+              required
+            ></v-text-field>
+
+            <v-btn
+              :disabled="!valid"
+              color="primary"
+              class="mr-4"
+              @click="submit"
+            >
+              Войти
+            </v-btn>
+          </v-form>
+        </v-card-text>
+      </v-card>
     </v-flex>
   </v-layout>
 </template>
 
 <script>
-
-export default {
-  sockets: {
-    connect: function () {
-      console.log('socket connected')
+  export default {
+    layout: 'empty',
+    sockets: {
+      connect: function () {
+        console.log('socket connected')
+      },
+      customEmit: function (data) {
+        console.log('this method was fired by the socket server. eg: io.emit("customEmit", data)')
+      }
     },
-    customEmit: function (data) {
-      console.log('this method was fired by the socket server. eg: io.emit("customEmit", data)')
-    }
-  },
-  methods: {
-    message() {
-      this.$socket.client.emit('createMessage', { text: "FROM CLIENT"});
-    }
+    data: () => ({
+      valid: true,
+      name: '',
+      nameRules: [
+        v => !!v || 'Введите имя',
+        v => (v && v.length <= 10) || 'Имя не должно превышать 10 символов',
+      ],
+      room: '',
+      roomRules: [
+        v => !!v || 'Введите комнату',
+      ]
+    }),
+
+    methods: {
+      submit () {
+        this.$refs.form.validate()
+      }
+    },
   }
-}
 </script>
